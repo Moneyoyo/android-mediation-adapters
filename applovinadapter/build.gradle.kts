72dbddfdfd3c7f9071f4182bbedbadd3fc30ab60
mnyytech <technical@moneyoyo.org>
2026-04-13 17:03:58 +0700

    build(applovin): move maven publish to libs.version, migrate to gradle daemon

--- applovinadapter/build.gradle.kts
+++ applovinadapter/build.gradle.kts
@@ -4,7 +4,7 @@ import com.vanniktech.maven.publish.SourcesJar
 
 plugins {
     alias(libs.plugins.android.library)
-    id("com.vanniktech.maven.publish") version "0.36.0"
+    alias(libs.plugins.maven.publish)
 }
 
 ext {
